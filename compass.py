723e317ef25a22b2c73d7a693fa547b42181b03c
Kien Luu <kienluu@kien56.local>
2013-01-11 13:04:03 +0000

    add production compile to compass

--- compass.py
+++ compass.py
@@ -1,6 +1,9 @@
 from fabric.api import env, run, task, cd
 
 @task
-def compass_compile():
+def compass_compile(DEV_ENV=False):
     with cd(env.PROJECT_PATH):
-        run('compass compile')
+        if DEV_ENV:
+            run('compass compile --force')
+        else:
+            run('compass compile --force -e production')
